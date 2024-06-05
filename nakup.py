d43c72b376a2eb9887c45280a421c1b44c1e9a20
ondra <horynaondrej@seznam.cz>
2024-06-05 19:24:12 +0200

    verze-2

--- nakup.py
+++ nakup.py
@@ -10,15 +10,28 @@ class Nakup:
         klic, 
         datum_klic, 
         exemplar_id, 
+        zamestnanec_id,
         cena
     ):
         self.klic = klic
         self.datum_klic = datum_klic
         self.exemplar_id = exemplar_id
+        self.zamestnanec_id = zamestnanec_id
         self.cena = cena
 
     def __str__(self):
         return f'Klíč: {self.klic}, ' \
             f'Datum: {self.datum_klic}, ' \
             f'Exemplář ID: {self.exemplar_id}, ' \
+            f'Zaměstnanec ID: {self.zamestnanec_id}, ' \
             f'Cena: {self.cena}'
+    
+    def format_csv(self):
+        return (
+            str(self.klic) + ';'
+            + str(self.datum_klic) + ';'
+            + str(self.exemplar_id) + ';'
+            + str(self.zamestnanec_id) + ';'
+            + str(self.cena)
+            )
+
--- readme.txt
+++ readme.txt
@@ -1,1 +1,8 @@
 git clone https://github.com/horynaondrej/Knihovna.git
+
+cd /
+git init
+git remote add origin <your-repo-url>
+git pull
+git checkout main -f
+git branch --set-upstream-to origin/main
